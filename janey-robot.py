1377b8405b81868feeb48f05685ba64430bfe4e1
Ian Mulvany <ian@l2284.mpl.root-domain.org>
2009-08-20 16:44:05 +0100

    stable v 2.0

--- janey-robot.py
+++ janey-robot.py
@@ -1,64 +1,207 @@
-#TODO try AppendMarkup
-#TODO try Annotation
-#  check the wire to see the annotation system if you want to do this
-# look at http://code.google.com/p/google-wave-resources/source/browse/trunk/samples/extensions/robots/python/syntaxy/syntaxy.py
-# for annotation use blip.SetAnnotation
-# look at http://code.google.com/p/downy/source/browse/downy.py?r=39a14157d5a7294ceee5bd1027b1b82c25dfceb3
-# look at http://code.google.com/p/msg-in-a-bottle-wave-robot/source/browse/trunk/miab.py for storing info on a wave
-
 from waveapi import events
-#from waveapi import model
 from waveapi import robot
-#from waveapi import document
 import re
 import logging
-from xml.dom import minidom
 import urllib
+from BeautifulSoup import BeautifulStoneSoup
 
 logger = logging.getLogger('janey-robot')
 logger.setLevel(logging.DEBUG)
 
-current_version = '1.1.33'
-HELP_MESSAGE = "\n\n I'll give you some help in a moment"
+current_version = '2.0'
+HELP_MESSAGE = "I query http://www.biosemantics.org/jane/, my commands are:  \
+                   (janey:journals) - returns a list of recommended journals\n \
+                   (janey:articles) - returns a list of related articles\n \
+                   (janey:authors) - returns a list of related authors\n \
+                   (janey:about) - gives a little info about me\n \
+                   (janey:help) - prints this message"
+                   
+ABOUT_MESSAGE = "I pass the content of the blip I am called from to the \
+Journal Name Author Estimator service, and I put some of the \
+response from that service into a reply blip. \
+You can get more info about the nice JANE people \
+at\n\n \
+http://www.biosemantics.org/jane/faq.php.\n\n \
+You can interact directly with their service at: \
+\nhttp://www.biosemantics.org/jane/\n\n \
+I was written by Ian Mulvany from Nature Publishing Group as a \
+proof of concept for the ScienceOnline London Conference, 2009.\n\n \
+You can invite him to your wave at ianmulvany@wavesandbox.com \n\n \
+My source code can be found at:\n\n \
+http://github.com/IanMulvany/janey-robot/tree/master\n\n \
+For help on my commands type (janey:help)"
 
-def OnRobotAdded(properties, context):
-    """
-    Invoked when the robot has been added.
-    """
-    HELLO_MESSAGE = "Hi, I'm janey-robot, let me help you find journals, \
-                        I'm version " + current_version
-    root_wavelet = context.GetRootWavelet()
-    root_wavelet.CreateBlip().GetDocument().SetText(HELLO_MESSAGE)
 
-def Notify(context):
-    """
-    We will only notify when the robot is added or updated, not 
-    when a new participant is added, this increases clutter in the wave
-       
-    """
-    root_wavelet = context.GetRootWavelet()
-    root_wavelet.CreateBlip().GetDocument().SetText("Hi everybody!")
+class journalInfo:
+    def __init__(self, name):
+        self.journalname =  name
+        self.rank = 0
+        self.score = 0
 
-def StripCommandFromBlipText(blipText, command):
-    """
-    returns the blip text with the command text removed
-    this function is also used to return the query string to be passed to JANE
+class authorInfo:
+    def __init__(self, name):
+        self.name =  name
+        self.rank = 0
+        self.score = 0
+        self.articles = []
 
+class articleInfo:
+    def __init__(self, pmid):
+        self.pmid = pmid
+        self.title = ""
+        self.rank = 0
+        self.score = 0
+        
+        
+def sort_results_by_rank(i,j):
     """
-    replace_string = "(janey:" + command + ")"
-    stripped_text = blipText.replace(replace_string,"")
-    return stripped_text
+    all root objects returned in the jane xml tree have a rank
+    and score attribute, this is a custom sort on rank
+    """
+    #print "in rank sorting"
+    if i.rank > j.rank:
+        return 1
+    elif j.rank == i.rank:
+        return 0
+    else: # j.rank > i.rank
+        return -1
 
-def StripCommandFromBlip(properties, context, blip_text, command):
+def sort_results_by_score(i,j):
     """
-    Take a blip and modify the blip in place
-    Remove the janey command string from the blip
+    all root objects returned in the jane xml tree have a rank
+    and score attribute, this is a custom sort on score
+    """
 
+    if i.score > j.score:
+        return 1
+    elif j.score == i.score:
+        return 0
+    else: # j.rank > i.rank
+        return -1
+
+def GetJournalInfo(soup):
+    journals = soup.findAll('journal')
+    return_results = []
+    for journal in journals:
+        title = journal.journalname.contents[0]
+        rank = journal['rank']
+        score = journal['score']
+        j = journalInfo(title)
+        j.score = int(score) # read as str, convert to int for sorting
+        j.rank = int(rank) # read as str, convert to int for sorting
+        return_results.append(j)
+    return return_results
+
+def GetArticleInfo(soup):    
+    articles = soup.findAll('article')
+    return_results = []
+    for article in articles:
+        # extract the info from the xml tree
+        title = article.title.contents[0]
+        rank = article['rank']
+        score = article['score']
+        pmid = article.pmid.contents[0]
+        # add the info into our article info class object
+        a = articleInfo(pmid)
+        a.score = int(score) # read as str, convert to int for sorting
+        a.rank = int(rank) # read as str, convert to int for sorting
+        a.title = title
+        return_results.append(a)
+    return return_results
+
+def GetAuthorInfo(soup):
+
+    tags = [tag for tag in soup.results.findAll(recursive=False)]
+    author_metric = tags[0::3]
+    author_names = tags[1::3]
+    author_evidence = tags[2::3]
+    
+    #print author_metric
+    
+    #print author_names
+    #print author_evidence
+    
+    return_results = []
+    for metric, name, evidence in zip (author_metric, author_names, author_evidence):
+        #print metric, name, evidence
+        
+        rank = metric['rank']
+        score = metric['score']
+        name = name.contents[0]
+        author_articles = GetArticleInfo(evidence)
+        evidence = evidence
+         
+        au = authorInfo(name)
+        au.score = int(score) # read as str, convert to int for sorting
+        au.rank = int(rank) # read as str, convert to int for sorting
+        au.articles = author_articles
+         
+        return_results.append(au)
+    return return_results
+
+def formatJournalResults(journal_results):
     """
-    logger.debug('stripping the command from the blip')    
-    blip = context.GetBlipById(properties['blipId'])
-    stripped_text = StripCommandFromBlipText(blip_text, command)
-    blip.GetDocument().SetText(stripped_text)
+    take journalname rank and score and format for printing
+    """
+    
+    journal_results.sort(sort_results_by_rank)
+    top_5_by_rank = journal_results[0:5]
+    journal_results.sort(sort_results_by_score)
+    top_5_by_score = journal_results[0:5]
+
+    text = "top 5 journals by score: "
+    for journal in top_5_by_score:
+        text = text + journal.journalname  + " " +  str(journal.score) + "\n"
+    
+    text = text + "\ntop 5 journals by rank: \n"
+    for journal in top_5_by_rank:
+        text = text + journal.journalname + " " + str(journal.rank) + "\n"      
+    return text
+    
+def genPubMedLinkFromPMID(pmid):
+    return "http://www.ncbi.nlm.nih.gov/pubmed/" + pmid
+
+def formatArticleResults(Results):
+    """
+    take article title  rank and score and format for printing
+    include pmid and generate a link to pubmed
+    """
+    
+    Results.sort(sort_results_by_rank)
+    top_5_by_rank = Results[0:5]
+    Results.sort(sort_results_by_score)
+    top_5_by_score = Results[0:5]
+    
+    text = "top 5 articles by score: "
+    for article in top_5_by_score:
+        text = text + article.title + " " +  str(article.score) + "\n"
+        pubmed_link = genPubMedLinkFromPMID(article.pmid)
+        text = text + article.pmid + "(" + pubmed_link + ")\n"
+    
+    text = text + "\ntop 5 articles by rank: \n"
+    for article in top_5_by_rank:
+        text = text + article.title + " " + str(article.rank) + "\n"
+        pubmed_link = genPubMedLinkFromPMID(article.pmid)
+        text = text + article.pmid + "(" + pubmed_link + ")\n"                    
+    return text
+
+def formatAuthorResults(Results):
+
+    Results.sort(sort_results_by_rank)
+    top_5_by_rank = Results[0:5]
+    Results.sort(sort_results_by_score)
+    top_5_by_score = Results[0:5]
+
+    text = "top 5 authors by score: "
+    for author in top_5_by_score:
+        text = text + author.name + " " +  str(author.score) + "\n"
+    
+    text = text + "\ntop 5 authors by rank: \n"
+    for author in top_5_by_rank:
+        text = text + author.name + " " + str(author.rank) + "\n"
+    
+    return text
+
 
 def QueryJaneAPI(command, query_text):
     """
@@ -79,15 +222,79 @@ def QueryJaneAPI(command, query_text):
     http://biosemantics.org:8080/jane/journals?text=malaria%20vaccines
 
     """
-    logger.debug('generating JANE API call URI')    
     jane_root_url = 'http://biosemantics.org:8080/jane/'
-    encoded_query_text = urllib.urlencode({'text':query_text})
-    query_url = jane_root_url + command + "/" + encoded_query_text
-    logger.debug(query_url)
-    #xml = minidom.parse(urllib.urlopen(query_url))
-    #journals = xml.getElementsByTagName('journalname')
-    #journal_names = "\n".join(journals)
-    return query_url
+    encoded_query_text = urllib.quote(query_text.rstrip().lstrip())
+    query_url = jane_root_url + command + "?text=" + encoded_query_text
+    print query_url
+
+    try:
+        html = urllib.urlopen(query_url)
+        document = html.read()
+    except:
+        # add error loggin here
+        return "oops, there was a problem calling the JANE service, sorry!"
+    try:
+        vanilla_doc = document.decode('us-ascii', 'ignore')
+        soup = BeautifulStoneSoup(vanilla_doc)
+    except:
+        # add error loggin here
+        return "oops, I chocked trying to process the returned xml, sorry!"
+    
+    return_text = ""
+    if command == "journals":
+        journal_results = GetJournalInfo(soup)
+        return_text = formatJournalResults(journal_results)
+    elif command == "authors":
+        author_results = GetAuthorInfo(soup)
+        return_text = formatAuthorResults(author_results)
+    elif command == "articles":
+        article_results = GetArticleInfo(soup)
+        return_text = formatArticleResults(article_results)
+                 
+    return return_text
+
+
+def OnRobotAdded(properties, context):
+    """
+    Invoked when the robot has been added.
+    """
+    HELLO_MESSAGE = "Hi, I'm janey-robot, let me help you find journals. For help type (janey:help) \
+ I'm version " + current_version
+    root_wavelet = context.GetRootWavelet()
+    root_wavelet.CreateBlip().GetDocument().SetText(HELLO_MESSAGE)
+
+def Notify(context):
+    """
+    We will only notify when the robot is added or updated, not 
+    when a new participant is added, this increases clutter in the wave
+       
+    """
+    root_wavelet = context.GetRootWavelet()
+    root_wavelet.CreateBlip().GetDocument().SetText("Hi everybody!")
+
+
+def StripCommandFromBlipText(blipText, command):
+    """
+    returns the blip text with the command text removed
+    this function is also used to return the query string to be passed to JANE
+
+    """
+    replace_string = "(janey:" + command + ")"
+    stripped_text = blipText.replace(replace_string,"")
+    return stripped_text
+
+
+def StripCommandFromBlip(properties, context, blip_text, command):
+    """
+    Take a blip and modify the blip in place
+    Remove the janey command string from the blip
+
+    """
+    logger.debug('stripping the command from the blip')    
+    blip = context.GetBlipById(properties['blipId'])
+    stripped_text = StripCommandFromBlipText(blip_text, command)
+    blip.GetDocument().SetText(stripped_text)
+
 
 def ReplyToBlipWithJaneInfo(properties, context, blip_text, command):
     """
@@ -97,6 +304,8 @@ def ReplyToBlipWithJaneInfo(properties, context, blip_text, command):
     """
     if command == 'help':
         response = HELP_MESSAGE
+    elif command == 'about':
+        response = ABOUT_MESSAGE
     elif command in ['authors', 'journals', 'articles']:
         query_text = StripCommandFromBlipText(blip_text, command)
         logger.debug('about to call JANE API')    
@@ -108,6 +317,7 @@ def ReplyToBlipWithJaneInfo(properties, context, blip_text, command):
         
     blip = context.GetBlipById(properties['blipId']) 
     blip.CreateChild().GetDocument().SetText(response)
+    
     
 def OnBlipSubmitted(properties, context):
     """
